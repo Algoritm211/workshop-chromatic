5659376ec1bf0f0627b98321bd71109488aca9f1
Alexey <algoritm211@gmail.com>
2022-05-09 11:14:27 +0300

    Fix button

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -19,7 +19,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button pro',
+  label: 'Button',
 };
 
 export const Secondary = Template.bind({});
